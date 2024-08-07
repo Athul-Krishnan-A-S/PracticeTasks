dfe9124425902675266a9bf3940ab449dd6ac94b
Athul-Krishnan-A-S <athulkrishnan@seemymachines.com>
2024-08-07 12:36:19 +0530

    new commit

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,16 @@
 import './button.css';
 
-function Button(props) {
+function Button({ value, onClick, smallButton, smallButtonOnlyBorder, largeButton, largeButtonOnlyBorder, hoverButton }) {
     return (
         <button
             className={
-                props.smallButton ? 'button small-button' :
-                    props.smallButtonOnlyBorder ? 'button small-button-only-border' :
-                        props.largeButton ? 'button large-button' :
-                            props.largeButtonOnlyBorder ? 'button large-button-only-border' :
-                                props.hoverButton ? 'button hover-button' : ''
+                (smallButton && 'button small-button') ||
+                (smallButtonOnlyBorder && 'button small-button-only-border') ||
+                (largeButton && 'button large-button') ||
+                (largeButtonOnlyBorder && 'button large-button-only-border') ||
+                (hoverButton && 'button hover-button')
 
-            } onClick={() => props.onClick(props.value)}>{props.value}</button>
+            } onClick={() => onClick(value)}>{value}</button>
     )
 }
 export default Button;
