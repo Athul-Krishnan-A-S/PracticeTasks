b65cf44157627a6d8daf672fa9e9a0c68a5a3852
Athul-Krishnan-A-S <athulkrishnan@seemymachines.com>
2024-08-07 12:44:59 +0530

    changed button component functionalities

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,16 @@
 import './button.css';
 
 function Button({ value, onClick, smallButton, smallButtonOnlyBorder, largeButton, largeButtonOnlyBorder, hoverButton }) {
+    let button = 'button';
+    button += (smallButton && ' small-button') || '';
+    button += (smallButtonOnlyBorder && ' small-button-only-border') || '';
+    button += (largeButton && ' large-button') || '';
+    button += (largeButtonOnlyBorder && ' large-button-only-border') || '';
+    button += (hoverButton && ' hover-button') || '';
     return (
-        <button
-            className={
-                (smallButton && 'button small-button') ||
-                (smallButtonOnlyBorder && 'button small-button-only-border') ||
-                (largeButton && 'button large-button') ||
-                (largeButtonOnlyBorder && 'button large-button-only-border') ||
-                (hoverButton && 'button hover-button')
 
-            } onClick={() => onClick(value)}>{value}</button>
+        <button
+            className={button} onClick={() => onClick(value)}>{value}</button>
     )
 }
 export default Button;
